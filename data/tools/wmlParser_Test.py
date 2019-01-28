0e91ad76de581556b7dab9ecc96c3ea15223201c
Lovens Weche <lweche159@gmail.com>
2019-01-28 18:10:16 -0500

    Initial test to use the wml parser implemented.
    Initial test to use the wml parser implemented.

---
+++ data/tools/wmlParser_Test.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+import wesnoth.wmlparser3 as wmlparser
+
+p = wmlparser.Parser()
+cfg = p.parse_file('wmlParser_Test_WMLFile.cfg')
+for unit in cfg.get_all(tag = "unit"):
+    print(unit.get_text_val("id"))
+    print(unit.get_text_val("name"))
+    for abilities in unit.get_all(tag = "abilitities"):
+        for ability in abilities.get_all(tag = ""):
+            print(ability.get_name())
+            print(ability.get_text_val("id"))
